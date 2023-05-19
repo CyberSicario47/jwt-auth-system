891936d24ca65cc0b1a8f1d06cc73c98584d4110
Ahmad <ali.ah.naeem2704@gmail.com>
2023-05-19 09:00:33 -0400

    ### implementing react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
 import Login from "./Screens/Login/Login";
+import "./index.css";
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter
 } from "react-router-dom";
 import Signup from "./Screens/Signup/Signup.jsx";
 import Home from "./Screens/Home/Home.jsx";
+import Root from "./routes/routes.jsx";
+import ErrorPage from "./Screens/Error/Error.jsx";
+import Contact from "./Screens/Contact/Contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/signup",
-    element: <Signup/>,
-  },
-  {
-    path: "/",
-    element: <Home/>,
-  },
+export const app = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
+    },
 ]);
 
-const App = () => <RouterProvider router={router}/>
-
-
-export default App
